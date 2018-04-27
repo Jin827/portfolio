24ba776e208f153b5ec3465f06d988fb6d76999c
Jin827 <hyojin.lee827@gmail.com>
2018-04-27 13:58:17 +0900

    button hover

--- client/resources/js/app.js
+++ client/resources/js/app.js
@@ -34,9 +34,10 @@
 
     btnToSubPortfolio.addEventListener('click', function(e) {
         e.preventDefault();
-
+    
         mainPortfolio.classList.add('fade');
         subPortfolio.classList.add('open');
+        
     }, false)
 
     btnToMainPortfolio.addEventListener('click', function(e) {
@@ -44,6 +45,70 @@
         
         mainPortfolio.classList.remove('fade');
         subPortfolio.classList.remove('open');
+    }, false)
+
+    /* --- button hover --- */
+    const btnBlue = document.querySelector('.js--btn-hover--blue');
+    const btnOrange = document.querySelector('.js--btn-hover--orange');
+    const btnBlueReverse = document.querySelector('.js--btn-hover--blue--reverse');
+    const linkedinIcon = document.querySelector('.js--btn-hover--linkedin');
+    const githubIcon = document.querySelector('.js--btn-hover--github');
+
+    const triangleDarkblue = 'resources/assets/svg/triangle-darkblue.svg';
+    const triangleBlue= 'resources/assets/svg/triangle.svg';
+    const triangleDarkorange = 'resources/assets/svg/triangle-reverse-darkorange.svg';
+    const triangleOrange = 'resources/assets/svg/triangle-reverse-orange.svg';
+    const triangleReverseDarkblue = 'resources/assets/svg/triangle-reverse-darkblue.svg';
+    const triangleReverseBlue= 'resources/assets/svg/triangle-reverse.svg';
+    const github = 'resources/assets/svg/icon/github.svg';
+    const githubDark = 'resources/assets/svg/icon/github-dark.svg';
+    const linkedin = 'resources/assets/svg/icon/linkedin.svg';
+    const linkedinDark = 'resources/assets/svg/icon/linkedin-dark.svg';
+
+
+    btnBlue.addEventListener('mouseover', function(e) {
+        e.preventDefault();
+        this.src = triangleDarkblue;
+    }, false)
+    btnBlue.addEventListener('mouseout', function(e) {
+        e.preventDefault();
+        this.src = triangleBlue;
+    }, false)
+
+    btnOrange.addEventListener('mouseover', function(e) {
+        e.preventDefault();
+        this.src = triangleDarkorange;
+    }, false)
+    btnOrange.addEventListener('mouseout', function(e) {
+        e.preventDefault();
+        this.src = triangleOrange;
+    }, false)
+
+    btnBlueReverse.addEventListener('mouseover', function(e) {
+        e.preventDefault();
+        this.src = triangleReverseDarkblue;
+    }, false)
+    btnBlueReverse.addEventListener('mouseout', function(e) {
+        e.preventDefault();
+        this.src = triangleReverseBlue;
+    }, false)
+
+    linkedinIcon.addEventListener('mouseover', function(e) {
+        e.preventDefault();
+        this.src = linkedinDark;
+    }, false)
+    linkedinIcon.addEventListener('mouseout', function(e) {
+        e.preventDefault();
+        this.src = linkedin;
+    }, false)
+
+    githubIcon.addEventListener('mouseover', function(e) {
+        e.preventDefault();
+        this.src = githubDark;
+    }, false)
+    githubIcon.addEventListener('mouseout', function(e) {
+        e.preventDefault();
+        this.src = github;
     }, false)
 
     /* --- Portfolio page change --- */
