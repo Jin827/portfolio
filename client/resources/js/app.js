60fe01390fce2960b3e64f08c87fac6fb5fc1c24
Jin827 <hyojin.lee827@gmail.com>
2018-05-24 15:34:42 +0900

    sub portfolio open/close event listener

--- client/resources/js/app.js
+++ client/resources/js/app.js
@@ -57,22 +57,23 @@
 
 
     /* --- Sub Prtfolio --- */
-    const btnToSubPortfolio = document.querySelector('.js--btn-to-sub');
-    const btnToMainPortfolio = document.querySelector('.js--btn-to-main');
+    const btnToSubPortfolio = document.querySelectorAll('.js--btn-to-sub');
+    const btnToMainPortfolio = document.querySelectorAll('.js--btn-to-main');
 
-    btnToSubPortfolio.addEventListener('click', function(e) {
+    btnToSubPortfolio.forEach( btn => btn.addEventListener('click', showSubPortfolio, false) );
+    btnToMainPortfolio.forEach( btn => btn.addEventListener('click', hideSubPortfolio, false) );
+
+    function showSubPortfolio(e) {
         e.preventDefault();
-        console.log('this.id: ', this.id);
         document.querySelector(`.main--${this.id}`).classList.add('fade');
         document.querySelector(`.sub--${this.id}`).classList.add('open');
-        
-    }, false)
+    };
 
-    btnToMainPortfolio.addEventListener('click', function(e) {
+    function hideSubPortfolio(e) {
         e.preventDefault();
         document.querySelector(`.main--${this.id}`).classList.remove('fade');
         document.querySelector(`.sub--${this.id}`).classList.remove('open');
-    }, false)
+    };
     
 
     /* --- Portfolio page change --- */
