eb3bd71fb014c57ddc7da98c0527b8838836a84e
Jin827 <25231717+Jin827@users.noreply.github.com>
2018-06-30 19:38:03 +0900

    gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,7 @@ const gulp = require('gulp');
 // Minification dependencies
 const minifyHTML = require('gulp-minify-html');
 const csso = require('gulp-csso');
-const uglify = require('gulp-uglify-es');
-// const composer = require('gulp-uglify/composer');
-// const uglify = composer(uglifyes, console);
+const uglify = require('gulp-uglify-es').default;
 const concat = require('gulp-concat');
 const imagemin = require('gulp-imagemin');
 
@@ -14,18 +12,48 @@ const browserSync = require('browser-sync').create();
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
 const pump = require('pump');
+const jshint = require('gulp-jshint');
+const stylish = require('jshint-stylish');
+const size = require('gulp-size');
+const notify = require('gulp-notify');
+const stripDebug = require('gulp-strip-debug');
 
-source = 'client/';
+source = 'development/';
 dist = 'production/';
 
-gulp.task('html', function() {
-  gulp.src(source + '*.html')
+gulp.task('imageoptim', () => {
+  gulp.src('client/resources/assets/img/*.{jpeg}')
+    .pipe(imagemin([
+      imagemin.gifsicle({interlaced: true}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.optipng({optimizationLevel: 5}),
+    ], {
+      verbose: true
+    }))
+    .pipe(gulp.dest(dist + 'client/resources/assets/img'))
+
+  gulp.src('client/resources/assets/svg/**/*.svg')
+    .pipe(imagemin([
+      imagemin.svgo({
+        plugins: [
+          {removeViewBox: true},
+          {cleanupIDs: false}
+        ]
+      })
+    ], {
+      verbose: true
+    }))
+    .pipe(gulp.dest(dist + 'client/resources/assets/svg'))
+})
+
+gulp.task('html', () => {
+  gulp.src('client/*.html')
     .pipe(minifyHTML())
     .pipe(gulp.dest(dist + 'client'));
 });
 
-gulp.task('css', function() {
-  gulp.src(source + 'resources/css/*.css')
+gulp.task('css', () => {
+  gulp.src('client/resources/css/*.css')
     .pipe(sourcemaps.init())
     .pipe(autoprefixer())
     .pipe(csso({
@@ -33,33 +61,79 @@ gulp.task('css', function() {
       sourceMap: true,
       debug: true
     }))
-    .pipe(sourcemaps.write())
+    .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest(dist + 'client/resources/css'))
+  
+  gulp.src('client/vendors/css/*.css')
+    .pipe(autoprefixer())
+    .pipe(csso({
+      restructure: false,
+      sourceMap: true,
+      debug: true
+    }))
+    .pipe(gulp.dest(dist + 'client/vendors/css'))
 })
 
-gulp.task('javascript', function (cb) {
+gulp.task('javascript', (cb) => {
   pump([
-    gulp.src(source + 'resources/js/*.js'),
+    gulp.src('client/resources/js/*.js'),
+    sourcemaps.init(),
+    stripDebug(),
     uglify(),
+    sourcemaps.write('./maps'),
     gulp.dest(dist + 'client/resources/js')
     ],
     cb
   ); 
-  // return gulp.src(source + 'resources/js/*.js')
-  //   .pipe(uglify())
-  //   .pipe(gulp.dest(dist + 'client/resources/js'))
+
+  pump([
+    gulp.src('client/vendors/js/*.js'),
+    stripDebug(),
+    uglify({}),
+    gulp.dest(dist + 'client/vendors/js')
+    ],
+    cb
+  ); 
+
+  pump([
+    gulp.src('server/*.js'),
+    sourcemaps.init(),
+    stripDebug(),
+    uglify({}),
+    concat('all.js'),
+    sourcemaps.write('./maps'),
+    gulp.dest(dist + 'server')
+    ],
+    cb
+  ); 
 });
 
-// gulp.task('serve', function() {
-//   browserSync.init({
-//     server: {
-//       baseDir: './client'
-//     },
-//   })
-//   // gulp.watch("client/*.html", [html]).on('change', browserSync.reload);
-//   // gulp.watch("client/resources/css/*.css", [css]);
-//   // gulp.watch('client/resources/js/*.js', [javascript])
-  
-// })
+// Scans JS files for errors
+gulp.task("jshint", () => {
+  return gulp.src(['client/resources/js/*.js', 'server/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'), {beep: true});
+});
 
-// gulp.task('default', ['html']);
+gulp.task('size', () => {
+  const s = size({
+    showFiles: true, 
+    pretty: true
+  });
+
+  gulp.src(dist + '/**/*')
+    .pipe(s)
+    .pipe(gulp.dest(dist))
+    .pipe(notify({
+      onLast: true,
+      message: () => `Total size ${s.prettySize}`
+    }))
+});
+
+gulp.task('browsersync', () => {
+  browserSync.init({
+    proxy: {
+      target: 'localhost:8080'
+    }
+  })
+})
