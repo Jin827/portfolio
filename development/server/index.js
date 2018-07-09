24729615552018e3d737a817e025883e07d24867
Jin827 <25231717+Jin827@users.noreply.github.com>
2018-07-10 01:54:32 +0900

    eslint

--- development/server/index.js
+++ development/server/index.js
@@ -12,16 +12,16 @@ const myApi = require('./api.js');
 
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({
-    extended: true
+	extended: true
 }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.post('/', (req, res) => {
 
-    return myApi.sendEmail(req.body)
-        .then(myApi.replyEmail(req.body))
-        .catch(err => console.log(err));
+	return myApi.sendEmail(req.body)
+		.then(myApi.replyEmail(req.body))
+		.catch(err => {console.log(err)});
 });
 
 app.use(express.static(`${process.cwd()}/development/client`));
@@ -29,34 +29,34 @@ app.use(express.static(`${process.cwd()}/development/client`));
 // error handlers
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // development error handler
 // will print stacktrace
 if (app.get('env') !== 'production') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.status('error', {
-            message: err.message,
-            error: err
-        });
-    });
+	app.use(function (err, req, res, next) {
+		res.status(err.status || 500);
+		res.status('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({
-        message: err.message,
-        error: {}
-    });
+	res.status(err.status || 500);
+	res.json({
+		message: err.message,
+		error: {}
+	});
 });
 
 app.listen(port, () => {
-    console.log(`Server is up on port ${port}!`);
+	console.log(`Server is up on port ${port}!`);
 });
 console.log(`NODE_ENV : ${process.env.NODE_ENV} mode`);
