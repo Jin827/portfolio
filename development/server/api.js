9279abadae64ede1d93be65617e2ef0d5b3645fa
Jin827 <25231717+Jin827@users.noreply.github.com>
2018-07-10 01:54:32 +0900

    eslint

--- development/server/api.js
+++ development/server/api.js
@@ -1,56 +1,56 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.GOOGLE_EMAIL,
-        pass: process.env.GOOGLE_PASSWORD
-    }
+	service: 'gmail',
+	auth: {
+		user: process.env.GOOGLE_EMAIL,
+		pass: process.env.GOOGLE_PASSWORD
+	}
 });
 
 module.exports = {
 
-    sendEmail: (data) => {
-        const {
-            name,
-            email,
-            subject,
-            message
-        } = data;
-        const mailOptions = {
-            from: 'Client via Portfolio <contact.portfolio@gmail.com>',
-            to: process.env.MY_EMAIL,
-            subject: subject,
-            html: `<p>Name: ${name} <br/> Contact: ${email}<br/><br/>${message}</p>`,
-        };
+	sendEmail: (data) => {
+		const {
+			name,
+			email,
+			subject,
+			message
+		} = data;
+		const mailOptions = {
+			from: 'Client via Portfolio <contact.portfolio@gmail.com>',
+			to: process.env.MY_EMAIL,
+			subject: subject,
+			html: `<p>Name: ${name} <br/> Contact: ${email}<br/><br/>${message}</p>`,
+		};
 
-        return new Promise((resolve, reject) => {
+		return new Promise((resolve, reject) => {
 
-                transporter.verify(function (error, success) {
-                    if (error) {
-                        console.log(error);
-                    } else {
-                        return transporter.sendMail(mailOptions, function (error, info) {
-                            error ?
-                                console.error(`Unable to send the email: ${error}`) :
-                                console.log(`Email sent: ${info.response}`);
-                        });
-                    }
-                });
-            })
-            .catch(err => console.error(`sendEmail func err : ${err}`));
-    },
+			transporter.verify(function (error, success) {
+				if (error) {
+					console.log(error);
+				} else {
+					return transporter.sendMail(mailOptions, function (error, info) {
+						error ?
+							console.error(`Unable to send the email: ${error}`) :
+							console.log(`Email sent: ${info.response}`);
+					});
+				}
+			});
+		})
+			.catch(err => console.error(`sendEmail func err : ${err}`));
+	},
 
-    replyEmail: (data) => {
-        const {
-            name,
-            email
-        } = data;
-        const mailOptions = {
-            from: 'Jiah Lee <donotreply@gmail.com>',
-            to: `${name} <${email}>`,
-            subject: 'Auto reply message from Jiah Lee',
-            html: `
+	replyEmail: (data) => {
+		const {
+			name,
+			email
+		} = data;
+		const mailOptions = {
+			from: 'Jiah Lee <donotreply@gmail.com>',
+			to: `${name} <${email}>`,
+			subject: 'Auto reply message from Jiah Lee',
+			html: `
                 <p>
                     Hello ${name},<br/><br/>
                     Thank you for getting in touch.<br/>
@@ -60,15 +60,15 @@ module.exports = {
                     Jiah Lee
                 </p>
             `
-        };
+		};
 
-        return new Promise((resolve, reject) => {
-                return transporter.sendMail(mailOptions, function (error, info) {
-                    error ?
-                        console.error(`Unable to send the email: ${error}`) :
-                        console.log(`Email sent: ${info.response}`);
-                });
-            })
-            .catch(err => console.error(`replyEmail func err : ${err}`));
-    }
+		return new Promise((resolve, reject) => {
+			return transporter.sendMail(mailOptions, function (error, info) {
+				error ?
+					console.error(`Unable to send the email: ${error}`) :
+					console.log(`Email sent: ${info.response}`);
+			});
+		})
+			.catch(err => console.error(`replyEmail func err : ${err}`));
+	}
 };
