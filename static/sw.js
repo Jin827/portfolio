6a10e4e1ffc65221e38b10929453556ece093b82
Jin827 <25231717+Jin827@users.noreply.github.com>
2018-07-17 23:34:09 +0900

    load google map API on scroll

--- static/sw.js
+++ static/sw.js
@@ -1,3 +1,4 @@
+
 if( 'function' === typeof importScripts) {
 	importScripts('/vendors/js/cache-polyfill.js');
 
@@ -8,30 +9,30 @@ if( 'function' === typeof importScripts) {
 					console.log('Opened cache');
 					return cache.addAll([
 						'/',
-						'/server/views/index.html',
+						//'/server/views/index.html',
 						'/resources/css/style.css',
 						'/resources/css/queries.css',
 						'/resources/js/app.js',
 					]);
-				})
+				}, err => console.error(err))
 		);
 	});
 }
 
-// self.addEventListener('fetch', (e) => {
-// 	console.log(`eventRequestUrl: ${e.request.url}`);
-// 	e.respondWith(
-// 		caches.match(e.request)
-// 			.then((response) => {
-// 				// Cache hit - return response
-// 				if (response) {
-// 					return response;
-// 				}
-// 				return fetch(e.request);
-// 			}
-// 			)
-// 	);
-// });
+self.addEventListener('fetch', (e) => {
+	console.log(`eventRequestUrl: ${e.request.url}`);
+	e.respondWith(
+		caches.match(e.request)
+			.then((response) => {
+				// Cache hit - return response
+				if (response) {
+					return response;
+				}
+				return fetch(e.request);
+			}
+			)
+	);
+});
 
 // self.addEventListener('activate', (e) => {
 
