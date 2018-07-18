51efc3a72cd081bcb3e34931287a0de61fbc714a
Jin827 <25231717+Jin827@users.noreply.github.com>
2018-07-18 20:38:20 +0900

    update resume link

--- static/sw.js
+++ static/sw.js
@@ -1,9 +1,10 @@
+
 if( 'function' === typeof importScripts) {
 	importScripts('/vendors/js/cache-polyfill.js');
 
 	self.addEventListener('install', (e) => {
 		e.waitUntil(
-			caches.open('sw-1')
+			caches.open('my-site-cache-v1')
 				.then((cache) => {
 					console.log('Opened cache');
 					return cache.addAll([
@@ -11,8 +12,6 @@ if( 'function' === typeof importScripts) {
 						'/resources/css/style.css',
 						'/resources/css/queries.css',
 						'/resources/js/app.js',
-						'/resources/assets/img/',
-						'/resources/assets/svg'
 					]);
 				}, err => console.error(err))
 		);
@@ -20,10 +19,11 @@ if( 'function' === typeof importScripts) {
 }
 
 self.addEventListener('fetch', (e) => {
-	// console.log(`eventRequestUrl: ${e.request.url}`);
+	console.log(`eventRequestUrl: ${e.request.url}`);
 	e.respondWith(
 		caches.match(e.request)
 			.then((response) => {
+				// Cache hit - return response
 				if (response) {
 					return response;
 				}
@@ -33,29 +33,19 @@ self.addEventListener('fetch', (e) => {
 	);
 });
 
-self.addEventListener('activate', (e) => {
+// self.addEventListener('activate', (e) => {
 
-	const cacheWhitelist = ['sw-1'];
+// 	const cacheWhitelist = ['my-site-cache-v1'];
 
-	e.waitUntil(
-		caches.keys().then((cacheNames) => {
-			return Promise.all(
-				cacheNames.map((cacheName) => {
-					if (cacheWhitelist.indexOf(cacheName) === -1) {
-						return caches.delete(cacheName);
-					}
-				})
-			);
-		})
-	);
-});
-
-// function unregister() {
-// 	if ('serviceWorker' in navigator) {
-// 		navigator.serviceWorker.unregister().then(() => {
-// 			console.log('ServiceWorker unregistration successful.');
-// 		});
-// 	}
-// }
-
-// unregister();
+// 	e.waitUntil(
+// 		caches.keys().then((cacheNames) => {
+// 			return Promise.all(
+// 				cacheNames.map((cacheName) => {
+// 					if (cacheWhitelist.indexOf(cacheName) === -1) {
+// 						return caches.delete(cacheName);
+// 					}
+// 				})
+// 			);
+// 		})
+// 	);
+// });
