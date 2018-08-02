67317f3153fa99f200a624b0c84908aee7701c91
Jin827 <25231717+Jin827@users.noreply.github.com>
2018-08-02 12:33:23 +0900

    update header

--- src/server/index.js
+++ src/server/index.js
@@ -27,9 +27,9 @@ app.use(cookieParser());
 app.use(function(req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, POST, DELETE');
-	res.header('Access-Control-Allow-Headers', 'Origin,Content-Type, Authorization');
+	// res.header('Access-Control-Allow-Headers', 'Origin,Content-Type, Authorization');
 	res.header('Access-Control-Allow-Credentials', true);
-	// res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 	next();
 });
 
